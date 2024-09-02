d2f2fbf4a5834912398e9c7e099e66ed44308a7f
surendra-sk <114029428+surendra-sk@users.noreply.github.com>
2024-09-03 02:30:53 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -74,4 +74,5 @@ def webhook():
         return jsonify({"status": "error", "message": "Invalid signature"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
